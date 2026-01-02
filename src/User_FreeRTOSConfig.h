bfaa70094fa524b50bc4e879ebfa44c642523e70
Jeremy Baker <jeremy.baker@renesas.com>
2026-01-02 16:28:06 +0000

    Add setting to enable use of Performance Monitor

---
+++ src/User_FreeRTOSConfig.h
@@ -0,0 +1,10 @@
+#ifndef USER_FREERTOSCONFIG_H_
+#define USER_FREERTOSCONFIG_H_
+
+void lv_freertos_task_switch_in(const char * name);
+void lv_freertos_task_switch_out(void);
+
+#define traceTASK_SWITCHED_IN()   lv_freertos_task_switch_in(pxCurrentTCB->pcTaskName)
+#define traceTASK_SWITCHED_OUT()  lv_freertos_task_switch_out()
+
+#endif /* USER_FREERTOSCONFIG_H_ */
